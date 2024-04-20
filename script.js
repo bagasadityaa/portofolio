ffa5897ffb830643738f9541ffb412fc0d71078e
bagas aditya <bagasaditya599@gmail.com>
2024-04-20 11:19:19 +0700

    update

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ setInterval(() => {
 document.addEventListener("DOMContentLoaded", function () {
   document.body.classList.add("loading-complete"); // Add loading-complete class to body when document is fully loaded
 });
+
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
+const popup = document.getElementById("popup");
+
+// Tambahkan event listener untuk tombol pembuka
+openBtn.addEventListener("click", () => {
+  popup.classList.remove("hidden"); // Tampilkan pop-up saat tombol pembuka diklik
+});
+
+// Tambahkan event listener untuk tombol penutup
+closeBtn.addEventListener("click", () => {
+  popup.classList.add("hidden"); // Sembunyikan pop-up saat tombol penutup diklik
+});
