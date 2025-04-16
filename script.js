876edb37117b59d47e60a7e7c11b5f851398282c
bagas aditya <bagasaditya599@gmail.com>
2025-04-16 22:17:14 +0700

    Enhance profile section: Add emojis for engagement, improve text clarity, and implement dynamic portfolio listing in JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 const slides = document.querySelectorAll(".slide");
 let currentSlide = 0;
+const portofolios = [
+  {
+    id: 1,
+    title: "Ramadhan Kareem",
+    image: "images/posterramadhan.svg",
+    description: "Aplikasi Web ramadhan",
+    date: "2024-02-10",
+  },
+  {
+    id: 2,
+    title: "My Anime List ",
+    image: "images/posterramadhan.svg",
+    description: "Aplikasi Web ramadhan",
+    date: "2024-03-01",
+  },
+];
 
+const container = document.getElementById("portofolio-list");
+
+portofolios.forEach((item) => {
+  container.innerHTML += `
+<div class="rounded-lg shadow-md p-4">
+  <img
+    src="${item.image}"
+    alt="${item.title}"
+    class="w-full h-40 object-cover rounded-md mb-2"
+  />
+  <h3 class="text-xl font-semibold text-gray-800">${item.title}</h3>
+  <p class="text-sm text-gray-500 mb-1">${item.date}</p>
+  <p class="text-gray-700">${item.description}</p>
+</div>
+`;
+});
 setInterval(() => {
   slides[currentSlide].classList.add("hidden");
   currentSlide = (currentSlide + 1) % slides.length;
@@ -10,20 +42,6 @@ setInterval(() => {
 document.addEventListener("DOMContentLoaded", function () {
   document.body.classList.add("loading-complete"); // Add loading-complete class to body when document is fully loaded
 });
-
-// const openBtn = document.getElementById(["openBtn"], ["openBtn1"]);
-// const closeBtn = document.getElementById(["closeBtn"], ["closeBtn1"]);
-// const popup = document.getElementById(["popup"], ["popup1"]);
-
-// // Tambahkan event listener untuk tombol pembuka
-// openBtn.addEventListener("click", () => {
-//   popup.classList.remove("hidden"); // Tampilkan pop-up saat tombol pembuka diklik
-// });
-
-// // Tambahkan event listener untuk tombol penutup
-// closeBtn.addEventListener("click", () => {
-//   popup.classList.add("hidden"); // Sembunyikan pop-up saat tombol penutup diklik
-// });
 
 // Dapatkan semua tombol pembuka dan tombol penutup
 const openBtns = document.querySelectorAll(".openBtn");
